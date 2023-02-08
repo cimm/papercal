c8c441634d7d4720041c2e4d628e20c4d3dc6eca
Simon Schoeters <b2282e9d-46e8-4ec4-946a-b6ad84078b1c@astil.be>
2023-02-08 11:11:13 +0100

    Nicer Free Sans font

--- papercal.ino
+++ papercal.ino
@@ -9,6 +9,8 @@
 #include <GxEPD2_BW.h>
 #include <time.h>
 #include <vector>
+#include <Fonts/FreeSans12pt7b.h>
+#include <Fonts/FreeSansBold12pt7b.h>
 
 const int padding = 5;
 PaperDevice device;
@@ -80,9 +82,7 @@ void events_to_display() {
     return;
   }
 
-  device_display.panel.setTextColor(GxEPD_BLACK);
-  device_display.panel.setTextSize(2);
-  device_display.panel.setCursor(0, 10 * padding);
+  device_display.panel.setFont(&FreeSans12pt7b);
   if (cal.error()) {
     error_to_display(cal.last_error_message.c_str());
   } else {
@@ -94,28 +94,25 @@ void events_to_display() {
 
 void event_to_display(Event event) {
   char formatted_start[8];
-  if (event.is_all_day()) {
-    device_display.panel.print("     ");
-  } else {
+  if (!event.is_all_day()) {
     device_display.panel.print(event.formatted_start("%H:%M").c_str());
+    device_display.panel.print(" ");
   }
-  device_display.panel.print(" ");
   std::string summary = event.summary();
-  if (summary.length() > 24) { summary = summary.substr(0, 25) + "-"; }
+  if (summary.length() > 24) { summary = summary.substr(0, 26) + "-"; }
   device_display.panel.println(summary.c_str());
 }
 
 void title_to_display(tm time_info) {
   char formatted_date[11];
   strftime(formatted_date, 11, "%a %e %b", &time_info);
-  device_display.panel.fillRect(0, 0, device_display.panel.width(), 8 * padding, GxEPD_BLACK);
-  device_display.panel.setTextColor(GxEPD_WHITE);
-  device_display.panel.setTextSize(4);
-  device_display.panel.setCursor(padding, padding);
+  device_display.panel.setCursor(0, 15);
+  device_display.panel.setFont(&FreeSansBold12pt7b);
   device_display.panel.println(formatted_date);
 }
 
 void error_to_display(const char* error_message) {
+  device_display.panel.setFont();
   device_display.panel.setTextSize(1);
   device_display.panel.setCursor(padding, device_display.panel.height() - 2 * padding);
   device_display.panel.print(error_message);
@@ -124,6 +121,7 @@ void error_to_display(const char* error_message) {
 void refresh_datetime_to_display() {
   char formatted_refresh_date[30];
   device_datetime.format("%d/%m %H:%M", formatted_refresh_date, sizeof(formatted_refresh_date));
+  device_display.panel.setFont();
   device_display.panel.setTextSize(1);
   device_display.panel.setCursor(device_display.panel.width() - 65 - padding, device_display.panel.height() - 2 * padding);
   device_display.panel.print(formatted_refresh_date);
