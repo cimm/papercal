77a2dc800616572c7af605bcd52fa6c0692d321d
Simon Schoeters <b2282e9d-46e8-4ec4-946a-b6ad84078b1c@astil.be>
2025-07-14 21:00:13 +0200

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "PaperCal Development Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default = let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        arduino-cli
+        python3Packages.pyserial
+      ];
+    };
+  };
+}
