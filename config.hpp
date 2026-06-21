cf8a5382d719a81b36f45ebb3927c3e9ddb97d62
Simon Schoeters <b2282e9d-46e8-4ec4-946a-b6ad84078b1c@astil.be>
2026-06-21 12:03:16 +0200

    Don't compile when configuration is not set
    I would compile and upload the sketch and monitor the serial connection
    to see... nothing. I spent a long time trying to understand why the
    serial connection did not work. Wrong baudrate? Wrong cable? Turns out
    I simply forgot to set the WiFi SSID and password in the config so the
    sketch would simply be trying to connect to the WiFi without telling me.
    This commit refuses to compile if the credentials are not given.

--- config.hpp
+++ config.hpp
@@ -14,3 +14,12 @@
 #define NTP_POOL "pool.ntp.org"
 
 #define REFRESH_ON_ERROR true  // Keep old events on screen if there is an error? No error messages will be shown on the display.
+
+constexpr bool _strings_equal(const char* a, const char* b) {
+  return *a == *b && (*a == '\0' || _strings_equal(a + 1, b + 1));
+}
+
+static_assert(!_strings_equal(WIFI_SSID, "ssid"), "WIFI_SSID is still set to the default value!");
+static_assert(!_strings_equal(WIFI_PASSWORD, "password"), "WIFI_PASSWORD is still set to the default value!");
+static_assert(!_strings_equal(CALENDAR_USER, "user"), "CALENDAR_USER is still set to the default value!");
+static_assert(!_strings_equal(CALENDAR_PASSWORD, "password"), "CALENDAR_PASSWORD is still set to the default value!");
